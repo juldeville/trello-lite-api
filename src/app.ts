2ded140845b9276544e2a212a03b4559640d8b8d
Julien <j.deville@protonmail.com>
2025-09-04 02:41:47 +0200

    added cors setup

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import express from "express";
 import authRouter from "./routes/auth";
 import indexRouter from "./routes/index";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3001", // your frontend
+    credentials: true, // only if you’ll use cookies
+  })
+);
 
 app.use(express.json());
 app.use("/", indexRouter);
